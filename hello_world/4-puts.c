4e06660b25da456a7031dc1b499749390c507f33
semaed <figsaned@gmail.com>
2023-02-17 18:01:31 +0000

    Correction of the code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** 
- * main - Entry point
+/**
+ * main - Prints "Programming building a multilingual puzzle, followed \n
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n"
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
