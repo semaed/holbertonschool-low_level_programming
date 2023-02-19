480ddc0730de9d32326fd955cfd792abd0e82607
semaed <figsaned@gmail.com>
2023-02-19 02:18:34 +0000

    2nd correction

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print and that piece of art is useful"
- * Return: Always 1 (Success)
+ * main - print and that piece of art is useful
+ * Return: Always 1
  * /
+ *
  * int main(void)
  * {
  * write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
