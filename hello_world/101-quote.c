b678b1c4225fe20088a76df407263dafd43b8f35
semaed <figsaned@gmail.com>
2023-02-19 02:21:50 +0000

    4th correction

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - print and that piece of art is useful
- * Return: Always 1
- */
+ *main - print and that piece of art
+ *Return: Always 1
  *
- * int main(void)
- * {
- * write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- * return (1);
- * }
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
