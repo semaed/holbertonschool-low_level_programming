01d41a90e4b1af1dad12ae5e115a0d21a2faa7df
semaed <figsaned@gmail.com>
2023-02-20 20:55:18 +0000

    Changin to capital L

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	last = n % 10;
 
 	if (last > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("last digit of %d is %d is 0", n, last);
 	else if (!(last < 6))
