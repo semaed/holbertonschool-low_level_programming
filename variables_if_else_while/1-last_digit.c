0eb542e40a63966b433c3e1da4bd814cf1f3789a
semaed <figsaned@gmail.com>
2023-02-20 20:30:08 +0000

    Create C program and give permission to execute

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** main - assing random number toa a variable
+ * Return: Always 0 (Success) 
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (last > 5)
+		printf("last digit of %d is %d and is greater than 5", n, last);
+	else if (last == 0)
+		printf("last digit of %d is %d is 0", n, last);
+	else if (!(last < 6));
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	return (0);
+}
+
