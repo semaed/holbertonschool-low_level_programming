b28de2570da72ca91eea4604be8218dc5e0dbb9e
semaed <figsaned@gmail.com>
2023-02-20 21:46:00 +0000

    Write C program give command to execute

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = "a";
+
+	while (alpha <= "z");
+
+	{
+		putchar(alpha);
+		alpha++
+	}
+	putchar("\n");
+
+	return (0);
+}
