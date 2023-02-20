8b503cd3907307e7c6de9c484f73a3f1f7e55b89
semaed <figsaned@gmail.com>
2023-02-20 18:43:56 +0000

    Adding standard i/o

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - assign a random number to the variable
@@ -12,6 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n>0)
 	{
 		printf("%d is positive\n", n);
