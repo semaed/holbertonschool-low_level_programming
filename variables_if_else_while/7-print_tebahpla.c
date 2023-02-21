97c009d0f32e64fe54b0affe5ef4aaab7fcee4da
semaed <figsaned@gmail.com>
2023-02-21 13:53:35 +0000

    Write C program and give permission to execute

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+		while (alpha >= 'z')
+		{
+			putchar(alpha);
+			alpha--;
+		}
+	return (0);
+}
