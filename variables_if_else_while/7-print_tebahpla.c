c8a82627a1fd8aefa808befe65c7db444f669e43
semaed <figsaned@gmail.com>
2023-02-21 14:04:38 +0000

    3rd correction of the code

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -7,14 +7,13 @@
 
 int main(void)
 {
-	char alpha = 'a';
+	char alpha = 'z';
 
-	while (alpha >= 'z')
+	while (alpha >= 'a')
 	{
 		putchar(alpha);
 		alpha--;
 	}
-		
 	putchar('\n');
 	return (0);
 }
