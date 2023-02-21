29d2123c9fd256a370820c20bfbcb60366e792eb
semaed <figsaned@gmail.com>
2023-02-21 16:23:38 +0000

    Write C program give permission to execute

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case hexadecimal
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+	char alpha;
+		for (number = 48; number <= 58; number++)
+		{
+			putchar(number);
+		}
+		for (alpha = 'a'; alpha <= 'f'; alpha++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+		return (0);
+}
