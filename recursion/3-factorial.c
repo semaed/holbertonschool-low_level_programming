8f0f53b860d80ef7af76e73ea4258cc7c334978f
semaed <figsaned@gmail.com>
2023-03-07 19:17:06 +0000

    Write C program and give permission to execute

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - return factorial of a given number
+ * @n: pointer
+ * Return: the factorial of a number
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+
+	}
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (n * factorial(n - 1));
+}
