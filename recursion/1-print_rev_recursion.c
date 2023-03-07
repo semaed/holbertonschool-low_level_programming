e15f60a7d3784ddad060ee0676cf773b49807d1c
semaed <figsaned@gmail.com>
2023-03-07 18:08:59 +0000

    Write C program and give permission to execute

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print strin in reverse
+ * @s: pointer
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
