49b99c3ff6b886d7ad4a6d06cd310ce180bf8d54
semaed <figsaned@gmail.com>
2023-03-15 22:07:09 +0000

    Write C program and give permission to execute

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to add
+ * Return: pointer with the address
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, i, j;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+	
+	if (n > len2)
+		n = len2;
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	len2 = 0;
+	for (j = i; j < (len1 + n); j++)
+	{
+		concat[j] = s2[len2];
+		len2++;
+	}
+	concat[j] = '\0';
+	return (concat);
+}
