b456b2364574ffc8bf7128cc9176735fb36a223c
semaed <figsaned@gmail.com>
2023-03-20 14:28:27 +0000

    Write headder file and give permission to execute

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - typedef variable
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ * Description: variables and typedef variable
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
