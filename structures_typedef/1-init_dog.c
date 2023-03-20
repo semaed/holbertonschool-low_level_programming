4ee0ab91298fe456cbf61ec2ac4976be7c560415
semaed <figsaned@gmail.com>
2023-03-20 17:09:07 +0000

    Correction to the code

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,10 +13,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		exit(0);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
-
-	if (d == NULL)
-		return (0);
