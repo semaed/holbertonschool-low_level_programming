cbad960371b8909c5323ddc54c51f5e3298bd987
semaed <figsaned@gmail.com>
2023-03-20 16:51:09 +0000

    Write C program and give permission to execute

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - typedef variable description
+ * @d: varibale
+ * @name: variable char
+ * @age: varibale float
+ * @owner: variable char
+ * Description: variable typedef and variables
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - typedef variable
+ * struct Repo:
+
+GitHub repository: holbertonschool-low_level_programming
+Directory: structures_typedef
+File: dog.hdog - typedef variable
  * @name: char type
  * @age: float type
  * @owner: char type
@@ -16,5 +20,6 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
