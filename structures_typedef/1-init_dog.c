cf655d63df221c8f84d7ca1e4cdc5ca4dacd048e
semaed <figsaned@gmail.com>
2023-03-20 17:28:25 +0000

    Correctio to the code

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,20 +3,22 @@
 #include <stdio.h>
 
 /**
- * init_dog - typedef variable description
+ * init_dog - typedef
  * @d: varibale
  * @name: variable char
  * @age: varibale float
  * @owner: variable char
- * Description: variable typedef and variables
+ *  Description: variable typedef and variables
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
 	if (d == NULL)
 	{
-		exit(0);
+		return;
 	}
+
 
 	d->name = name;
 	d->age = age;
