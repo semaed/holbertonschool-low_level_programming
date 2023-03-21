5ba8f2130833dbbbba9fed35aec4fc847e5d93e2
semaed <figsaned@gmail.com>
2023-03-21 14:41:58 +0000

    Write C program and give permission to execute

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * new_dog - create new dog
+ * @name: char variable
+ * @age: int variable
+ * @owner: char variable
+ * Return: pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
+	if (nd == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+	nd->name = malloc(strlen(name + 1));
+		if (nd->name == NULL)
+		{
+			free(nd->name);
+			return (NULL);
+		}
+	nd->owner = malloc(strlen(owner + 1));
+		if (nd->owner == NULL)
+		{
+			free(nd->owner);
+			return (NULL);
+		}
+	nd->age = age;
+	nd->name = strcpy(nd->name, name);
+	nd->owner = strcpy(nd->owner, owner);
+
+	return (nd);
+}
