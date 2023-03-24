e0af04b946dc4936996ca3d986db19850a8e9c96
semaed <figsaned@gmail.com>
2023-03-24 19:31:28 +0000

    git push

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,17 +13,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list (list);
-	va_start(list,n);
+	char *ptr;
+	
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (list == NULL)
+		ptr = "(nil)";
+		if (list != NULL)
 		{
-		     printf("(nil)");
+			printf("%s", va_arg(list, char *));
 		}
-			
-			printf("%s", va_arg(list, char*));
-		
+
+		if (list == NULL)	
+		{
+		     printf("%s", ptr);
+		}
 
 		if (separator != NULL && i < n - 1)
 		{
