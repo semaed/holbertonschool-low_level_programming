8cf09a9a54cd977e6e1120d63ba4e898f4260a13
semaed <figsaned@gmail.com>
2023-03-24 17:52:49 +0000

    Write C program and give permission to execute

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_string - prototype
+ * @separator: pointer
+ * @n: variable
+ * Return:
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list (list);
+	va_start(list,n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (list != NULL)
+		{
+			printf("%s", va_arg(list, char*));
+		}
+		
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+
+	}
+			va_end(list);
+			printf("\n");
+}
