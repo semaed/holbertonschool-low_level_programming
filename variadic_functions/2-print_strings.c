cfe20f69a7e11face30cd66cc699abc3be5bfe23
semaed <figsaned@gmail.com>
2023-03-24 19:32:45 +0000

    git push

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", va_arg(list, char *));
 		}
 
-		if (list == NULL)	
+		else	
 		{
 		     printf("%s", ptr);
 		}
