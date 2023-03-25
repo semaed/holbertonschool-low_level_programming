e9d13562292b3bf8ca209eab5102a9e20f3649a8
semaed <figsaned@gmail.com>
2023-03-25 02:42:16 +0000

    Write C program and give permission to execute

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,22 +1,49 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
-
 /**
- *
- *
- *
+ * print_all - function
+ * @format: list of types
  */
-
 void print_all(const char * const format, ...)
 {
-	int i;
-	int len = strlen(format);
-	va_list list;
-	i = 0;
+	va_list args;
+	int i = 0, h = 0;
+	char *s = NULL;
 
-	while (i < len)
+	va_start(args, format);
+	while (format && format[h])
+		h++;
+	while (format && format[i])
 	{
-		switch (format[i]);
-
-
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i != h - 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
