1e63bf349a3682489078fb619382e6642eeb9cca
semaed <figsaned@gmail.com>
2023-04-04 00:20:40 +0000

    Fix Betty Style on code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_list -  prints all the elements of a list_t list.
- * @h: Adress of the head of the linked list to print
+ * @h: Adress of the head of the list to print
  * Return: Unsigned int with the number of nodes printed
  */
 size_t print_list(const list_t *h)
