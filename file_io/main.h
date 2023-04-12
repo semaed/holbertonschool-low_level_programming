ca6d0ec18ee34d073631112306c4c5ca20677976
semaed <figsaned@gmail.com>
2023-04-12 01:49:41 +0000

    Fix code

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* prototypes for all the functions */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
