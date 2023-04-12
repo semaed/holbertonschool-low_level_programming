da18105b54be4402e9b2e4eaa2e736f253731567
semaed <figsaned@gmail.com>
2023-04-12 01:22:21 +0000

    Write C function that returns the number of bits you would need to flip to get from one number to another

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another
+ * @n: The number
+ * @m: Number to flip n to
+ * Return:Necessary number of bits to flip to get from n to m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+	return (bits);
+}
