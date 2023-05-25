3a8ba348b258b3d51d39e33ec10989f9a33a8614
semaed <figsaned@gmail.com>
2023-05-25 11:53:24 +0000

    Add a headers file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stddef.h>
+
+/*Functions prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
+
+#endif
